e6a40305f4a699902044a9f49d00d624505b8adc
EmreDemirci0 <yemre.1268@gmail.com>
2022-02-20 21:22:57 +0300

    Flappy-Bird
    Flappy Bird Yükledik

---
+++ Assets/Scripts/Kontrol.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Kontrol : MonoBehaviour
+{
+    public Sprite[] kuşSprite;
+    SpriteRenderer spriteRenderer;
+    bool ileriKontrol = true;
+    public float hiz_Tersorantılı;
+    int kusSayac = 0;
+    float kusAnimasyonZaman = 0;
+    Rigidbody2D fizik;
+    public int ZıplamaHız;
+
+    int puan = 0;
+    public Text PuanText;
+
+    bool oyunbitti = false;
+
+    OyunKontrol oyunKontrol;
+
+    AudioSource[] sesler;
+    public int enYuksekPuan = 0;
+    public int score = 0;
+    
+  
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        fizik = GetComponent<Rigidbody2D>();
+        
+        oyunKontrol = GameObject.FindGameObjectWithTag("oyunkontrol").GetComponent<OyunKontrol>();
+       sesler = GetComponents<AudioSource>();
+        enYuksekPuan = PlayerPrefs.GetInt("enYuksekPuanKayit");
+        score = PlayerPrefs.GetInt("puanKayit");
+        Debug.Log("En Yuksek puan"+ enYuksekPuan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    animasyonn();
+        if ((Input.GetMouseButtonDown(0)|| Input.GetKeyDown(KeyCode.Space)) && oyunbitti==false)
+        {
+            fizik.velocity = new Vector2(0,0);//Yerçekimi dolayısıyla artan hızı 0lar
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,ZıplamaHız));
+            sesler[0].Play();
+            
+        }
+        if (fizik.velocity.y>0)//Yani zıplarken
+        {
+            transform.eulerAngles = new Vector3(0,0,25);
+        }
+        else if (fizik.velocity.y < 0)//Yani düşerken
+        {
+            transform.eulerAngles = new Vector3(0, 0,-25);
+        }
+        
+
+    }
+   void animasyonn()
+    {
+        kusAnimasyonZaman += Time.deltaTime;
+        if (kusAnimasyonZaman > hiz_Tersorantılı)
+        {
+            kusAnimasyonZaman = 0;
+            if (ileriKontrol == true)
+            {
+                spriteRenderer.sprite = kuşSprite[kusSayac];// 0 1 2
+                kusSayac++;
+                if (kusSayac == kuşSprite.Length)
+                {
+                    kusSayac--; //2
+                    ileriKontrol = false;
+                }
+            }
+            else
+            {
+                kusSayac--;
+                spriteRenderer.sprite = kuşSprite[kusSayac];//1 0 
+                if (kusSayac == 0)
+                {
+                    //kusSayac++;
+                    ileriKontrol = true;
+                    //Debug.Log("Sayaç=" + kusSayac);
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag==("puan"))
+        {
+            puan++;
+            PuanText.text = puan.ToString();
+            sesler[1].Play();
+
+        }
+        if (col.gameObject.tag == "engel") 
+        {   
+
+            oyunbitti = true;
+            oyunKontrol.oyunbitti();
+            sesler[2].Play();
+            GetComponent<CapsuleCollider2D>().enabled =false;
+            if (puan>enYuksekPuan)
+            {
+                enYuksekPuan = puan;
+                PlayerPrefs.SetInt("enYuksekPuanKayit",enYuksekPuan);
+
+            }
+            Invoke("anaMenuyeDon",2);
+            //2 sn sonra anaMenuyeDon metodunu çağır
+
+   //         SceneManager.LoadScene("AnaMenu");
+
+
+        }
+    }
+    void anaMenuyeDon()
+    {
+        PlayerPrefs.SetInt("puanKayit",puan);
+        SceneManager.LoadScene("AnaMenu");
+    }
+}
+
+
+
+
---
+++ Assets/Scripts/OyunKontrol.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OyunKontrol : MonoBehaviour
+{
+    public GameObject gokyuzuBir;
+    public GameObject gokyuzuİki;
+    public float Arkaplan_Hız = 2f;
+
+    Rigidbody2D fizikBir;
+    Rigidbody2D fizikİki;
+
+    float uzunluk = 0;
+
+    public GameObject engel;
+    public int kacAdetEngel = 5;
+    GameObject[] engeller;
+    float degisimZaman = 0;
+    int sayac = 0;
+    public float BoruAralık = 1f;
+    AudioSource sourcwe;
+
+    bool oyunBittiMi = false;
+
+    void Start()
+    {
+        sourcwe = GetComponent<AudioSource>();
+        fizikBir = gokyuzuBir.GetComponent<Rigidbody2D>();
+        fizikİki = gokyuzuİki.GetComponent<Rigidbody2D>();
+
+        fizikBir.velocity = new Vector2(-Arkaplan_Hız+0.001f, 0);
+        fizikİki.velocity = new Vector2(-Arkaplan_Hız, 0);
+
+        uzunluk = gokyuzuBir.GetComponent<BoxCollider2D>().size.x;
+
+        engeller = new GameObject[kacAdetEngel];
+        for (int i = 0; i < engeller.Length; i++)
+        {
+            /***??**/
+            engeller[i] = Instantiate(engel, new Vector2(-20, -20), Quaternion.identity);
+            Rigidbody2D fizikEngel = engeller[i].AddComponent<Rigidbody2D>();
+            fizikEngel.gravityScale = 0;
+            fizikEngel.velocity = new Vector2(-Arkaplan_Hız, 0);
+        }
+
+    }
+    void Update()
+    {
+        if (oyunBittiMi == false)
+        {
+            if (gokyuzuBir.transform.position.x <= -uzunluk)
+            {
+                gokyuzuBir.transform.position += new Vector3(uzunluk * 2, 0);
+            }
+            else if (gokyuzuİki.transform.position.x <= -uzunluk)
+            {
+                gokyuzuİki.transform.position += new Vector3(uzunluk * 2, 0);
+            }
+            /**********************************************************************/
+            degisimZaman += Time.deltaTime;
+            if (degisimZaman > BoruAralık)
+            {
+                degisimZaman = 0;
+                //if (sayac < engeller.Length)
+                //{
+                float Y_eksenim = Random.Range(-3f, -1.05f);
+                engeller[sayac].transform.position = new Vector3(9.3f, Y_eksenim);
+                sayac++;
+                //}
+                //else
+                //  sayac = 0;
+                if (sayac >= engeller.Length)
+                {
+                    sayac = 0;
+                }
+            }
+        }
+        else
+        {
+            sourcwe.Stop();
+        }
+    }
+    public void oyunbitti()
+    {
+        for (int i = 0; i < engeller.Length; i++)
+        {
+            engeller[i].GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            fizikBir.velocity = Vector2.zero;
+            fizikİki.velocity = Vector2.zero;
+
+        }
+        oyunBittiMi = true;
+    }
+
+}
+
+
+
+
+
